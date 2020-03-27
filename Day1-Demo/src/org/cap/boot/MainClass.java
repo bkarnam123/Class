ca2104b3ac6a4023409b7748a7d57c8e7274e402
bkarnam123 <62607349+bkarnam123@users.noreply.github.com>
2020-03-27 22:28:17 +0530

    this is first comment line

---
+++ Day1-Demo/src/org/cap/boot/MainClass.java
@@ -0,0 +1,10 @@
+package org.cap.boot;
+import org.cap.model.Sales;
+
+public class MainClass {
+public static void main(String args[]) {
+	Sales obj=new Sales();
+	obj.print();
+	
+}
+}
---
+++ Day1-Demo/src/org/cap/model/Sales.java
@@ -0,0 +1,11 @@
+package org.cap.model;
+
+public class Sales {
+
+int [] arr= {3,45,67,-90,2};
+public void print() {
+	for(int i=0;i<arr.length;i++)
+		System.out.println(arr[i]);
+}
+
+}
